9c12c9a99f8af59d85f687633b86c0cc97a57acc
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-06-05 12:16:55 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,12 +4,8 @@ data = scraperwiki.scrape('https://www.sos.state.co.us/pubs/charities/reports/20
 reader = csv.DictReader(data.splitlines())
  
 for row in reader:
- print row
- scraperwiki.sqlite.save(['Ref. No.'], row)
- 
-for row in reader:
      for key, value in row.iteritems():
          print key
          print value
-print row['Ref. No.']
-scraperwiki.sqlite.save(['Ref. No.'], row)
+print row['Paid Solicitor']
+scraperwiki.sqlite.save(['Paid Solicitor'], row)
